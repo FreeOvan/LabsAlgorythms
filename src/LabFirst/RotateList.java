265f37471e0d1a9e92df71b917918d2c47764003
FreeOvan <artem-vagin-2005@mail.ru>
2025-02-20 05:29:22 +0500

    Initial commit

---
+++ src/LabFirst/RotateList.java
@@ -0,0 +1,27 @@
+package LabFirst;
+
+public class RotateList {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null) {
+            return head;
+        }
+        int length = 1;
+        ListNode tail = head;
+        ListNode temp = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+        k %= length;
+        if (k == 0){
+            return head;
+        }
+        for(int i = 0; i < length - k - 1; i++){
+            temp = temp.next;
+        }
+        ListNode newList = temp.next;
+        temp.next = null;
+        tail.next = head;
+        return newList;
+    }
+}
